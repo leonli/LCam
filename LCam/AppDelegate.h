175b24c9df7f5ec04e8e4200f12450b3969576c7
L <leon@baobaoj.com>
2014-04-04 17:16:21 +0800

    Initial Commit

---
+++ LCam/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LCam
+//
+//  Created by Leon Li on 4/4/14.
+//  Copyright (c) 2014 ooo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ LCamTests/LCamTests.m
@@ -0,0 +1,34 @@
+//
+//  LCamTests.m
+//  LCamTests
+//
+//  Created by Leon Li on 4/4/14.
+//  Copyright (c) 2014 ooo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LCamTests : XCTestCase
+
+@end
+
+@implementation LCamTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
